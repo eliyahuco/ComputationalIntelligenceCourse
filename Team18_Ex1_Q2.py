90a6def67dc01ffbde42374b0399cdb19c87a3f4
eliya <eliyahu.m.c@gmail.com>
2024-07-05 01:53:44 +0300

    working on main function

--- Team18_Ex1_Q2.py
+++ Team18_Ex1_Q2.py
@@ -1,6 +1,7 @@
 """Authors: Eliyahu cohen, id 304911084
             Daniel liberman, id 208206946
-
+both authors contributed equally to this assignment
+we were working together on the assignment and we both participated in the writing of the code and the writing of the report
 ---------------------------------------------------------------------------------
 Short Description:
 # Daniel Leiberman - 208206946, Eliyahu Cohen - 304911084
@@ -12,12 +13,17 @@ Offers good balance between exploration and exploitation
 Has fewer parameters to tune compared to other algorithms
 Known for good performance in continuous optimization problems
 We also considered the best set of parameters for each algorithm using a grid search
+the grid search function was implemented
+The grid search function takes the algorithm, parameter grid, number of particles, and number of iterations as input
+It then iterates over all possible combinations of parameters and returns the best set of parameters and the corresponding cost
 
+
+in the PSO algorithm, we used the following parameters:
 w (inertia weight): Balances exploration and exploitation. Values like 0.5, 0.7, and 0.9 are common choices because they help maintain diversity in the swarm and prevent premature convergence.
 c1 (cognitive component): Represents the particle's tendency to return to its best-known position. Values like 1.0, 1.5, and 2.0 are chosen based on empirical studies showing they effectively guide particles.
 c2 (social component): Represents the particle's tendency to move towards the global best position. Similar values to c1 are chosen for balance.
-For the BA algorithm:
 
+For the BA algorithm:
 f_min and f_max (frequency range): Control the pulse emission rates. Choosing values like 0, 0.5, 1, and 2 provides a range of behaviors from no pulse emission to frequent pulses.
 A (loudness): Controls the exploration capacity. Values like 0.5, 0.7, and 0.9 represent different degrees of exploration.
 r (pulse rate): Represents the rate at which bats emit pulses. Values like 0.1, 0.3, and 0.5 provide a range from sparse to frequent pulses.
@@ -36,6 +42,8 @@ def cost_func(x):
 
 # Constraints definition & check
 def constraints(x):
+    #t_s, t_h, <=99, r, l <=200
+
     Ts, Th, R, L = x
     g1 = -Ts + 0.0193 * R  # Cylinder thickness constraint
     g2 = -Th + 0.00954 * R  # Hemisphere thickness constraint
@@ -165,6 +173,7 @@ def grid_search(algorithm, param_grid, num_particles, num_iterations):
     best_params = None
     best_score = float('inf')
     for params in product(*param_grid.values()):
+
         param_dict = dict(zip(param_grid.keys(), params))
         if algorithm.__name__ == 'pso':
             _, score, _ = algorithm(num_particles, num_iterations, **param_dict)
@@ -183,22 +192,33 @@ def main():
     # Grid search for PSO
     pso_param_grid = {'w': [0.5, 0.7, 0.9], 'c1': [1.0, 1.5, 2.0], 'c2': [1.0, 1.5, 2.0]}
     best_pso_params, best_pso_score = grid_search(pso, pso_param_grid, num_particles, num_iterations // accuracy_param)
-    print(f"\nBest PSO parameters: {best_pso_params}")
-    print(f"Best PSO cost: {best_pso_score}")
 
     # Run PSO with best parameters and print optimal values
     best_position, best_score, pso_history = pso(num_particles, num_iterations, **best_pso_params)
-    print(f"\nOptimal PSO values: T_s={best_position[0]}, T_h={best_position[1]}, R={best_position[2]}, L={best_position[3]}")
+    print(f'\nPSO found a solution after {num_iterations} iterations')
+    print(f'f_cost = {best_score}')
+    print(f'T_s = {best_position[0]}')
+    print(f'T_h = {best_position[1]}')
+    print(f'R = {best_position[2]}')
+    print(f'L = {best_position[3]}')
+    print(f'g =[{[-best_position[0] + 0.0193*best_position[2], -best_position[1] + 0.00954*best_position[2], -np.pi*best_position[2]**2*best_position[3] - (4/3)*np.pi*best_position[2]**3 + 1_296_000, best_position[3] - 240]}]')
+    print(f"\nBest PSO parameters: {best_pso_params}")
 
     # Grid search for Bat Algorithm
     ba_param_grid = {'fmin': [0, 0.5], 'fmax': [1, 2], 'A': [0.5, 0.7, 0.9], 'r': [0.1, 0.3, 0.5]}
     best_ba_params, best_ba_score = grid_search(bat_algorithm, ba_param_grid, num_particles, num_iterations // accuracy_param)
-    print(f"\nBest Bat Algorithm parameters: {best_ba_params}")
-    print(f"Best Bat Algorithm cost: {best_ba_score}")
 
     # Run Bat Algorithm with best parameters and print optimal values
     best_position, best_score, ba_history = bat_algorithm(num_particles, num_iterations, **best_ba_params)
     print(f"\nOptimal Bat Algorithm values: T_s={best_position[0]}, T_h={best_position[1]}, R={best_position[2]}, L={best_position[3]}")
+    print(f'But Algorithm found a solution after {num_iterations} iterations')
+    print(f'f_cost = {best_score}')
+    print(f'T_s = {best_position[0]}')
+    print(f'T_h = {best_position[1]}')
+    print(f'R = {best_position[2]}')
+    print(f'L = {best_position[3]}')
+    print(f'g =[{[-best_position[0] + 0.0193*best_position[2], -best_position[1] + 0.00954*best_position[2], -np.pi*best_position[2]**2*best_position[3] - (4/3)*np.pi*best_position[2]**3 + 1_296_000, best_position[3] - 240]}]')
+    print(f"\nBest Bat Algorithm parameters: {best_ba_params}")
 
     plt.figure(figsize=(10, 6))
     plt.plot(pso_history, label='PSO')
