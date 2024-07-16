2d330dbb830de174ed686f800d09ff0b8bbf8c4e
eliya <eliyahu.m.c@gmail.com>
2024-07-16 21:39:18 +0300

    working on main function

--- EX2/Team18_Ex2_Q2.py
+++ EX2/Team18_Ex2_Q2.py
@@ -25,96 +25,151 @@ Provide a summary of the performance of each method and assess whether a linear 
 import numpy as np
 from sklearn.model_selection import train_test_split
 import pickle
+import template4wine as t4w
 
 data = pickle.load(file=open('wine_red_dataset.pkl', "rb"))
 X = data['features'] # ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
 Y = data['quality'] # [Quality]
-K = data['feature_names'] # Strings of feature names
+X_train, X_test, y_train, y_test = t4w.train_test_split(X, Y, test_size=0.2, random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-def normal_equation(X, y):
-    ''' Normal Equation method to solve for theta
+
+
+
+
+def Loss(y, y_pred):
     '''
-    X = np.hstack((np.ones((X.shape[0], 1)), X))
-    theta = np.linalg.inv(X.T @ X) @ X.T @ y
-    return theta
+    This function calculates the loss function
+    :param y: the true values
+    :param y_pred: the predicted values
+    :return: the loss value
+    '''
+    M = len(y)
+    S = 0
+    for i in range(M):
+        S += (y[i] - y_pred[i])**2
+    return (1/M) * S
 
-def gradient_descent(X, y, learning_rate=0.0001, epochs=1000):
-    X = np.hstack((np.ones((X.shape[0], 1)), X))
-    m = X.shape[0]
-    n = X.shape[1]
-    theta = np.zeros(n)
+def dLoss_dW(x, y, y_pred):
+    '''
+    This function calculates the derivative of the loss function
+    :param x: the input data
+    :param y: the true values
+    :param y_pred: the predicted values
+    :return: the derivative of the loss function
+    '''
+    M = len(y)
+    S = 0
+    for i in range(M):
+        S += -x[i] * (y[i] - y_pred[i])
+    return (2/M) * S
+
+def predict(W, X):
+    '''
+    This function predicts the values of the output
+    :param W: the weights
+    :param X: the input data
+    :return: the predicted values
+    '''
+    Y_p = []
+    for x in X:
+        Y_p.append(np.dot(W,x))
+    return np.array(Y_p)
+
+def normal_equation(X_train,y_train):
+    '''
+    This function calculates the weights using the normal equation without using the pinv function
+    :param X_train: the training data
+    :param y_train: the training labels
+    :return: the weights
+    '''
+    x = np.array(X_train)
+    y = np.array(y_train)
+    inverse_x_xT = np.linalg.inv(np.dot(x.T,x))
+    pseudo_inverse = np.dot(inverse_x_xT,x.T)
+    W = np.dot(pseudo_inverse,y)
+    return W
+
+def gradient_descent(X_train,y_train,X_test,y_test,learning_rate = 0.1,epochs= 1000):
+    '''
+    This function calculates the weights using the gradient descent method
+    :param X_train: the training data
+    :param y_train: the training labels
+    :param X_test: the test data
+    :param y_test: the test labels
+    :param learning_rate: the learning rate
+    :param epochs: the number of epochs
+    :return: the weights, the loss on the test data, the loss on the training data
+    '''
+    Wu = np.random.randn(X_train.shape[1]) # Initial weigth vector
+    L_train = []
+    L_test = []
     for i in range(epochs):
-        gradient = (1/m) * X.T @ (X @ theta - y)
-        theta = theta - learning_rate * gradient
-    return theta
+        Y_p = predict(Wu, X_train)
+        Wu = Wu - learning_rate * dLoss_dW(X_train, y_train ,Y_p) # Update weights
+        L_train.append(Loss(y_train, Y_p))
+        L_test.append(Loss(y_test, predict(Wu, X_test)))
+    return Wu,L_test[-1],L_train[-1]
 
-def stochastic_gradient_descent(X, y, learning_rate=0.0001, epochs=1000, batch_size=1):
-    X = np.hstack((np.ones((X.shape[0], 1)), X))
-    m = X.shape[0]
-    n = X.shape[1]
-    theta = np.zeros(n)
+
+def get_batch(X, y, batch_size = 500):
+    ix = np.random.choice(X.shape[0], batch_size)
+    return X[ix, :], y[ix]
+
+def stochastic_gradient_descent(X_train,y_train,X_test,y_test,learning_rate = 0.1,epochs= 1000,batch_size = len(X_train)//2):
+    '''
+    This function calculates the weights using the stochastic gradient descent method
+    :param X_train: the training data
+    :param y_train: the training labels
+    :param X_test: the test data
+    :param y_test: the test labels
+    :param learning_rate: the learning rate
+    :param epochs: the number of epochs
+    :param batch_size: the size of the batch
+    :return: the weights, the loss on the test data, the loss on the training data
+    '''
+    Wu = np.random.randn(X_train.shape[1]) # Initial weigth vector
+    L_train = []
+    L_test = []
     for i in range(epochs):
-        for j in range(0, m, batch_size):
-            X_batch = X[j:j+batch_size]
-            y_batch = y[j:j+batch_size]
-            gradient = (1/batch_size) * X_batch.T @ (X_batch @ theta - y_batch)
-            theta = theta - learning_rate * gradient
-    return theta
+        X_batch, y_batch = get_batch(X_train, y_train,batch_size)
+        Y_p = predict(Wu, X_batch)
+        Wu = Wu - learning_rate * dLoss_dW(X_batch, y_batch ,Y_p) # Update weights
+        L_train.append(Loss(y_batch, Y_p))
+        L_test.append(Loss(y_test, predict(Wu, X_test)))
+    return Wu,L_test[-1],L_train[-1]
 
-def loss(X, y, theta):
-    X = np.hstack((np.ones((X.shape[0], 1)), X))
-    m = X.shape[0]
-    return np.sum((X @ theta - y) ** 2) / (2 * m)
+def main():
+    '''
+    This function runs the main code
+    '''
+    W_normal = normal_equation(X_train,y_train)
+    pred_quality = []
+    for i in range(len(X_test)):
+        pred_quality.append(np.dot(W_normal,X_test[i]))
+    NEL = Loss(y_test,pred_quality)
+    print('#'*100)
+    print('The loss on the test data using the normal equation is:',NEL)
+    print('The weights using the normal equation are:',W_normal)
+    print('#'*100)
+    Wu,loss_test,loss_train = gradient_descent(X_train,y_train,X_test,y_test,learning_rate=0.001,epochs=200)
+    print('The loss on the test data using the gradient descent method is:',loss_test)
+    print('The loss on the training data using the gradient descent method is:',loss_train)
+    print('The weights using the gradient descent method are:',Wu)
+    print('#'*100)
+    SWu,loss_test,loss_train = stochastic_gradient_descent(X_train,y_train,X_test,y_test,learning_rate=0.001,epochs=300,batch_size=200)
+    print('The loss on the test data using the stochastic gradient descent method is:',loss_test)
+    print('The loss on the training data using the stochastic gradient descent method is:',loss_train)
+    print('The weights using the stochastic gradient descent method are:',SWu)
 
-theta_normal = normal_equation(X_train, y_train)
-theta_gradient = gradient_descent(X_train, y_train)
-theta_stochastic = stochastic_gradient_descent(X_train, y_train)
 
-loss_normal = loss(X_test, y_test, theta_normal)
-loss_gradient = loss(X_test, y_test, theta_gradient)
-loss_stochastic = loss(X_test, y_test, theta_stochastic)
 
-print('Normal Equation Loss:', loss_normal)
-print('Gradient Descent Loss:', loss_gradient)
-print('Stochastic Gradient Descent Loss:', loss_stochastic)
 
-epochs = 0
-theta_gradient = np.zeros(X_train.shape[1] + 1)
-while True:
-    theta_gradient_new = gradient_descent(X_train, y_train, epochs=1)
-    if np.allclose(theta_gradient, theta_gradient_new, atol=1e-5):
-        break
-    theta_gradient = theta_gradient_new
-    epochs += 1
+if __name__ == '__main__':
+    main()
 
-print('Number of epochs for Gradient Descent to converge:', epochs)
-print('Theta:', theta_gradient)
 
-# Experiment with parameters like learning rate, batch size, and the number of epochs to achieve a satisfactory result.
-learning_rates = [0.0001, 0.001, 0.01, 0.1]
-batch_sizes = [1, 10, 100, 1000]
-epochs = [100, 1000, 10000]
 
-for lr in learning_rates:
-    for bs in batch_sizes:
-        for e in epochs:
-            theta = stochastic_gradient_descent(X_train, y_train, learning_rate=lr, epochs=e, batch_size=bs)
-            loss_sgd = loss(X_test, y_test, theta)
-            print('Learning Rate:', lr, 'Batch Size:', bs, 'Epochs:', e, 'Loss:', loss_sgd)
-            if loss_sgd < 0.5:
-                break
 
-# Provide a summary of the performance of each method and assess whether a linear model is suitable for predicting wine quality in this specific case.
-"""
-Summary:
-The Normal Equation method achieved the lowest loss on the test data, followed by Stochastic Gradient Descent and Gradient Descent. The number of epochs required for Gradient Descent to converge to results close to the Normal Equation was 1000. The best results were obtained with a learning rate of 0.01, batch size of 1, and 100 epochs for Stochastic Gradient Descent.
 
-Assessment:
-A linear model may not be suitable for predicting wine quality in this specific case, as the features may not have a linear relationship with the target variable. A more complex model, such as a polynomial regression or a machine learning algorithm like Random Forest or Gradient Boosting, may provide better results.
-"""
-# ---------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------
+
