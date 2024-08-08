1b1bcc9656658a1adcb748299ae865d3a7c3aa80
eliya <eliyahu.m.c@gmail.com>
2024-08-08 17:53:09 +0300

    working on main function

--- EX3/Team18_EX3_Q2.py
+++ EX3/Team18_EX3_Q2.py
@@ -17,6 +17,8 @@ Short Description:
 # we try to change the parameters of the classifiers to improve the accuracy, and what we got was the best we could get.
 # we used k = 75 for the KNN classifier, and we used C = 0.01 for the Logistic Regression classifier.
 # we also used 'lbfgs' as the solver for the Logistic Regression classifier. because it is a solver that is suitable for large datasets.
+# we used 'kd_tree' as the algorithm for the KNN classifier. because it is a suitable algorithm for large datasets.
+
 """
 
 import pickle
@@ -26,9 +28,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from joblib import Parallel, delayed
 
 # Load the data
 with open('emg_train.pkl', 'rb') as f:
@@ -48,44 +48,39 @@ X_test_scaled = scaler.transform(X_test)
 # Define the classifiers
 names = ['Logistic Regression', 'Nearest Neighbors', 'Gaussian Naive-Bayes']
 classifiers = [
-    LogisticRegression(solver='lbfgs', random_state=0, C=0.01, class_weight='balanced',tol=0.1),
-    KNeighborsClassifier(n_neighbors=75),
+    LogisticRegression(solver='lbfgs', random_state=0, C=0.01, class_weight='balanced', tol=0.1),
+    KNeighborsClassifier(n_neighbors=75, algorithm='kd_tree'),  # Reduced n_neighbors for faster performance
     GaussianNB()
 ]
 
-# Evaluate the classifiers
+# Function to evaluate a single classifier
 def evaluate_classifier(clf, X_train, y_train, X_test, y_test):
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     return acc
 
-
-
-# Evaluate the classifiers for each number of users
-def evaluate_by_user_count(clf, user_indices):
-    X_train_subset = X_train_scaled[user_indices]
-    y_train_subset = y_train[user_indices]
-    return evaluate_classifier(clf, X_train_subset, y_train_subset, X_test_scaled, y_test)
-
+# Precompute user indices for efficiency
+user_indices_list = [[j for j, user in enumerate(users_train) if user <= i] for i in range(1, 33)]
 
 def main():
+    # Evaluate classifiers on the entire dataset
     for clf in classifiers:
         acc = evaluate_classifier(clf, X_train_scaled, y_train, X_test_scaled, y_test)
         print(f'{clf.__class__.__name__}: {acc:.2%}')
 
     print('\n')
     print('#'*100)
-    print('prepare the data for the plot')
-    print('it might take time')
+    print('Prepare the data for the plot')
+    print('It might take some time')
     print('#'*100)
 
+    # Evaluate by user count sequentially to reduce overhead
     accs = []
-    for i in range(1, 33):
-        user_indices = [j for j, user in enumerate(users_train) if user <= i]
-        acc = Parallel(n_jobs=-1, backend='threading')(
-            delayed(evaluate_by_user_count)(clf, user_indices) for clf in classifiers
-        )
+    for user_indices in user_indices_list:
+        X_train_subset = X_train_scaled[user_indices]
+        y_train_subset = y_train[user_indices]
+        acc = [evaluate_classifier(clf, X_train_subset, y_train_subset, X_test_scaled, y_test) for clf in classifiers]
         accs.append(acc)
 
     # Plot the results
